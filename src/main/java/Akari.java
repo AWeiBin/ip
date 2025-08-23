86019d9769ec68a1bf477d9abde99cd5b278167d
AWeiBin <177638742+AWeiBin@users.noreply.github.com>
2025-08-23 22:28:07 +0800

    Rename chatbot to Akarin and add greeting and exit messages

---
+++ src/main/java/Akari.java
@@ -0,0 +1,18 @@
+public class Akari {
+
+    public static void main(String[] args) {
+        String chatbotName = "Akari";
+
+        String greetMessage =
+            "____________________________________________________________\n" +
+            "Hello! I'm " + chatbotName + "\n" +
+            "What can I do for you?\n" +
+            "____________________________________________________________";
+        String exitMessage =
+            "Bye. Hope to see you again soon!\n" +
+            "____________________________________________________________";
+
+        System.out.println(greetMessage);
+        System.out.println(exitMessage);
+    }
+}
